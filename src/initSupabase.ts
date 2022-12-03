06924b30f600403f1382605d10aeac8297ebb24e
Elijah Cannon <Cclayelijah@gmail.com>
2022-12-03 10:36:54 -0700

    setup

--- src/initSupabase.ts
+++ src/initSupabase.ts
@@ -1,8 +1,11 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_KEY } from "@env";
 
 // Better put your these secret keys in .env file
-export const supabase = createClient("supabaseUrl", "supabaseKey", {
+const supabaseUrl = "https://nyeesvbdynkaiyrnljwd.supabase.co";
+const supabaseKey = SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey, {
   localStorage: AsyncStorage as any,
-  detectSessionInUrl: false // Prevents Supabase from evaluating window.location.href, breaking mobile
+  detectSessionInUrl: false, // Prevents Supabase from evaluating window.location.href, breaking mobile
 });
